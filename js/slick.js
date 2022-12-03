4ed8d8180a8dc524bdc3f632a1478ef8ca01d8dc
mir_faisal <mirfaisal08@gmail.com>
2022-12-04 00:44:55 +0600

    added reviews part and slick add

---
+++ js/slick.js
@@ -0,0 +1,20 @@
+$(".slider-for").slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  fade: true,
+  asNavFor: ".slider-nav",
+  autoplay: true,
+  autoplaySpeed: 3000,
+});
+$(".slider-nav").slick({
+  centerMode: true,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  asNavFor: ".slider-for",
+  dots: false,
+  centerMode: true,
+  focusOnSelect: true,
+  autoplay: true,
+  autoplaySpeed: 3000,
+});
