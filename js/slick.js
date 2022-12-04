16c97e57fcde13a6e6632572177cd4f83182f12f
mir_faisal <mirfaisal08@gmail.com>
2022-12-05 03:46:19 +0600

    added reviewa part and some design is changes

--- js/slick.js
+++ js/slick.js
@@ -6,6 +6,35 @@ $(".slider-for").slick({
   asNavFor: ".slider-nav",
   autoplay: true,
   autoplaySpeed: 2000,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+      },
+    },
+    {
+      breakpoint: 575.98,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        fade: true,
+      },
+    },
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ],
 });
 $(".slider-nav").slick({
   centerMode: true,
@@ -17,4 +46,7 @@ $(".slider-nav").slick({
   arrows: false,
   autoplay: true,
   autoplaySpeed: 2000,
-});
+}
+
+);
+
