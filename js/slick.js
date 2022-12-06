23bfe6beb7ece71097805060201dfdfbd1856abf
mir_faisal <mirfaisal08@gmail.com>
2022-12-06 15:47:11 +0600

    changed some background

--- js/slick.js
+++ js/slick.js
@@ -8,22 +8,6 @@ $(".slider-for").slick({
   autoplaySpeed: 2000,
   responsive: [
     {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        infinite: true,
-        dots: true,
-      },
-    },
-    {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2,
-      },
-    },
-    {
       breakpoint: 575.98,
       settings: {
         slidesToShow: 1,
